afb19dfd86598336d0e6a6b1474dffbaaa6cb879
israelwalden <waldenisraelk@gmail.com>
2024-01-18 18:55:01 +0000

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    cl_args = sys.argv
+    if (len(cl_args) -1) == 0:
+        print("0 arguments")
+    elif (len(cl_args) -1) == 1:
+         print("1 argument")
+    else:
+        print(f"{(len(cl_args) - 1):d} arguments:")
+        for i in range(1, len(cl_args)):
+            print(f"{i:d}: {cl_args[i]}")
