3e2dd1b2fbe55232a131739d63a4c73fe0035737
israelwalden <waldenisraelk@gmail.com>
2024-01-18 19:03:52 +0000

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ import sys
 
 if __name__ == "__main__":
     cl_args = sys.argv
-    if (len(cl_args) -1) == 0:
+    if (len(cl_args) - 1) == 0:
         print("0 arguments")
-    elif (len(cl_args) -1) == 1:
-         print("1 argument")
+    elif (len(cl_args) - 1) == 1:
+        print("1 argument")
     else:
         print(f"{(len(cl_args) - 1):d} arguments:")
         for i in range(1, len(cl_args)):
