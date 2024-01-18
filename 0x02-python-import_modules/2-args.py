31917185dab6f0ca7e00fa4698a356832c1db95e
israelwalden <waldenisraelk@gmail.com>
2024-01-18 19:10:50 +0000

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 import sys
-
 if __name__ == "__main__":
     cl_args = sys.argv
-    if (len(cl_args) - 1) == 0:
-        print("0 arguments")
-    elif (len(cl_args) - 1) == 1:
-        print("1 argument")
+    if (len(cl_args)) == 1:
+        print("0 arguments.")
+    elif (len(cl_args)) == 2:
+        print("1 argument:")
     else:
         print(f"{(len(cl_args) - 1):d} arguments:")
-        for i in range(1, len(cl_args)):
-            print(f"{i:d}: {cl_args[i]}")
+    for i in range(1, len(cl_args)):
+        print(f"{i:d}: {cl_args[i]}")
