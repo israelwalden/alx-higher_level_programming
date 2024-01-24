101cfb9a02f33f8a62daf55e51faa973486d856e
israelwalden <waldenisraelk@gmail.com>
2024-01-24 17:00:05 +0000

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [list(map(lambda x: x ** 2, _)) for _ in matrix]
-    return new_matrix
+    return list(map(lambda x: list(map(lambda y: y ** 2, x)), matrix))
