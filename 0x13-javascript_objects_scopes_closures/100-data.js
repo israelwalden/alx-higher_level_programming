8b68a20e912c044557e536f5de42c8495fe35163
israelwalden <waldenisraelk@gmail.com>
2024-04-24 04:50:18 +0000

    task 11

--- 0x13-javascript_objects_scopes_closures/100-data.js
+++ 0x13-javascript_objects_scopes_closures/100-data.js
@@ -1,6 +1,2 @@
 #!/usr/bin/node
-const{ list } = require('./100-data');
-const nlist = list.map((value,index) => value * index);
-
-console.log("Initial list:", list);
-console.log("New list:", nlist);
+exports.list = [1, 2, 3, 4, 5];
